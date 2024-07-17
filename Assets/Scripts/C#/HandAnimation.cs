1837123957550e5598a275a70e48f58122797016
Anastasiya Panasyuk <42992862+mjolnika@users.noreply.github.com>
2024-07-17 22:54:15 +0300

    iloveyou

---
+++ Assets/Scripts/C#/HandAnimation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
